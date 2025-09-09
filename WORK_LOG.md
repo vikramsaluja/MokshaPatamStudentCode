20648abff8cdf2121572b6d5f1cd75cbbc767e35
vikramsaluja <vikram.saluja@menloschool.org>
2025-09-09 12:57:28 -0700

    Began to turn some psudocode into real code.

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -6,6 +6,7 @@ Name: Vikram Saluja
 | Sept 3  |  2:30-3pm   | I began thinking about how to approach the problem and trying to understand the game more. I went between thinking about begining the problem from the start vs at the end and then working down. |
 | Sept 3  | 7:15-7:50pm |                                At this point I do not think that starting at one end will particularly make a difference. I have now began to think about use BFS and queues to solve this puzzle |
 | Sept 4  | 7:45-8:15pm |                                                                              I began to write psudocode to attempt to solve the puzzle using BFS. I am stuck on how to handle the ladders portion |
+| Sept 9  | 11:35-11:55 |                       I began to start and turn some of my psudocode into real code. I did not get very far, but I intialized some of the variables at the got the actual coding portion started. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 import static java.lang.Math.*;
 /**
  * Moksha Patam
@@ -16,10 +19,18 @@ public class MokshaPatam {
      *  to reach the final square on a board with the given size, ladders, and snakes.
      */
     public static int fewestMoves(int boardsize, int[][] ladders, int[][] snakes) {
-        int sideLength = (int) sqrt(boardsize);
+
+        int[] layout = new int[boardsize + 1];
+
+        for(int i = 1; i <= boardsize + 1; i++){
+            // Set each square to lead to itself by default
+            layout[i] = i;
+        }
 
         // Create a queue of an array of ints[]
-        // Create lost of booleans to keep track visited squares
+        // Create list of booleans to keep track visited squares
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[] visitedSquare = new boolean[boardsize + 1];
 
         // Set up BFS queue: --> first in first out
         // Start with square 1 and add it to the queue
